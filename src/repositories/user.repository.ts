ec3e44316c1e788740f910dd8b35e9280b1a2bad
VrublevskiyVolodymyr <v637904@gmail.com>
2024-09-17 21:38:35 +0300

    change homework_4

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,71 +1,70 @@
-import {ApiError} from "../errors/api.error";
-import {fsService} from "../services/fs.service";
-import {IUser} from "../interfaces/user.interface";
+import { ApiError } from "../errors/api.error";
+import { IUser } from "../interfaces/user.interface";
+import { fsService } from "../services/fs.service";
 
 class UserRepository {
-    public async getList(): Promise<IUser[]> {
-        return await fsService.read();
+  public async getList(): Promise<IUser[]> {
+    return await fsService.read();
+  }
+
+  public async create(dto: Partial<IUser>): Promise<IUser> {
+    const users = await fsService.read();
+    const index = users.findIndex((user) => user.email === dto.email);
+    if (index !== -1) {
+      throw new ApiError("User with this email already exists", 409);
+    }
+    const newUser = {
+      id: users[users.length - 1].id + 1,
+      name: dto.name,
+      email: dto.email,
+      password: dto.password,
+    };
+    users.push(newUser);
+    await fsService.write(users);
+    return newUser;
+  }
+
+  public async getUser(userId: number): Promise<IUser> {
+    const users = await fsService.read();
+    const user = users.find((user) => user.id === userId);
+    if (!user) {
+      throw new ApiError("User not found", 404);
+    }
+    return user;
+  }
+
+  public async update(dto: IUser, userId: number): Promise<IUser> {
+    const users = await fsService.read();
+    const userIndex = users.findIndex((user) => user.id === userId);
+
+    if (userIndex === -1) {
+      throw new ApiError("User not found", 404);
     }
 
-    public async create(dto: IUser): Promise<IUser> {
-        const users = await fsService.read();
-        const index = users.findIndex((user) => user.email === dto.email);
-        if (index !== -1) {
-            throw new ApiError("User with this email already exists", 409);
-        }
-        const newUser = {
-            id: users[users.length - 1].id + 1,
-            name: dto.name,
-            email: dto.email,
-            password: dto.password,
-        };
-        users.push(newUser);
-        await fsService.write(users);
-        return newUser;
+    const updatedUser = { ...users[userIndex], ...dto };
+    users[userIndex] = updatedUser;
+
+    await fsService.write(users);
+
+    return updatedUser;
+  }
+
+  public async delete(
+    userId: number,
+  ): Promise<{ message: string; status: number }> {
+    const users = await fsService.read();
+    const index = users.findIndex((user) => user.id === userId);
+    if (index === -1) {
+      throw new ApiError("User not found", 404);
     }
+    users.splice(index, 1);
+    await fsService.write(users);
 
-    public async getUser(userId: number): Promise<IUser> {
-        const users = await fsService.read();
-        const user = users.find(user => user.id === userId);
-        if (!user) {
-            throw new ApiError("User not found", 404);
-        }
-        return user;
-    }
-
-    public async update(dto: IUser, userId: number): Promise<IUser> {
-        const users = await fsService.read();
-        const userIndex = users.findIndex(user => user.id === userId);
-
-        if (userIndex === -1) {
-            throw new ApiError("User not found", 404);
-        }
-
-        const updatedUser = {...users[userIndex], ...dto};
-        users[userIndex] = updatedUser;
-
-        await fsService.write(users);
-
-        return updatedUser;
-    }
-
-
-    public async delete(userId: number): Promise<{ message: string; status: number }> {
-        const users = await fsService.read();
-        const index = users.findIndex(user => user.id === userId);
-        if (index === -1) {
-            throw new ApiError("User not found", 404);
-        }
-        users.splice(index, 1);
-        await fsService.write(users);
-
-        return {
-            message: `User with id ${userId} is deleted`,
-            status: 204
-        };
-    }
-
-
+    return {
+      message: `User with id ${userId} is deleted`,
+      status: 204,
+    };
+  }
 }
 
 export const userRepository = new UserRepository();
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,71 +3,66 @@ import { IUser } from "../interfaces/user.interface";
 import { userRepository } from "../repositories/user.repository";
 
 class UserService {
-    public async getList(): Promise<IUser[]> {
-        return await userRepository.getList();
+  public async getList(): Promise<IUser[]> {
+    return await userRepository.getList();
+  }
+
+  public async getUser(userId: number): Promise<IUser> {
+    if (userId < 0 || Number.isNaN(userId)) {
+      throw new ApiError("Not valid ID", 400);
     }
 
-    public async getUser(userId:number): Promise<IUser> {
-        if (userId < 0 || Number.isNaN(userId)) {
-            throw new ApiError('Not valid ID', 400);
-        }
+    return await userRepository.getUser(userId);
+  }
 
-        return await userRepository.getUser(userId);
+  public async create(dto: Partial<IUser>): Promise<IUser> {
+    const { name, email, password } = dto;
+
+    if (!name || name.length < 3) {
+      throw new ApiError(
+        "Name is required and should be at least 3 characters",
+        400,
+      );
+    }
+    if (!email || !email.includes("@")) {
+      throw new ApiError("Email is required and should be valid", 400);
+    }
+    if (!password || password.length < 6) {
+      throw new ApiError(
+        "Password is required and should be at least 6 characters",
+        400,
+      );
+    }
+    return await userRepository.create(dto);
+  }
+
+  public async update(dto: IUser, userId: number): Promise<IUser> {
+    const { name, email, password } = dto;
+
+    if (userId < 0 || Number.isNaN(userId)) {
+      throw new ApiError("Not valid ID", 400);
     }
 
-    public async create(dto: IUser): Promise<IUser> {
-        const { name, email, password } = dto;
-
-        if (!name || name.length < 3) {
-            throw new ApiError(
-                "Name is required and should be at least 3 characters",
-                400,
-            );
-        }
-        if (!email || !email.includes("@")) {
-            throw new ApiError("Email is required and should be valid", 400);
-        }
-        if (!password || password.length < 6) {
-            throw new ApiError(
-                "Password is required and should be at least 6 characters",
-                400,
-            );
-        }
-        return await userRepository.create(dto);
+    if (name && name.length < 3) {
+      throw new ApiError("Name should be at least 3 characters", 400);
     }
-
-    public async update(dto: IUser, userId:number): Promise<IUser> {
-        const { name, email, password } = dto;
-
-        if (userId < 0 || Number.isNaN(userId)) {
-            throw new ApiError('Not valid ID', 400);
-        }
-
-        if (name && name.length < 3) {
-            throw new ApiError(
-                "Name should be at least 3 characters",
-                400,
-            );
-        }
-        if (email && !email.includes("@")) {
-            throw new ApiError("Email should be valid", 400);
-        }
-        if (password && password.length < 6) {
-            throw new ApiError(
-                "Password should be at least 6 characters",
-                400,
-            );
-        }
-        return await userRepository.update(dto, userId);
+    if (email && !email.includes("@")) {
+      throw new ApiError("Email should be valid", 400);
     }
-
-    public async delete(userId:number): Promise<{ message: string; status: number }> {
-        if (userId < 0 || Number.isNaN(userId)) {
-            throw new ApiError('Not valid ID', 400);
-        }
-        return await userRepository.delete(userId);
+    if (password && password.length < 6) {
+      throw new ApiError("Password should be at least 6 characters", 400);
     }
+    return await userRepository.update(dto, userId);
+  }
 
+  public async delete(
+    userId: number,
+  ): Promise<{ message: string; status: number }> {
+    if (userId < 0 || Number.isNaN(userId)) {
+      throw new ApiError("Not valid ID", 400);
+    }
+    return await userRepository.delete(userId);
+  }
 }
 
 export const userService = new UserService();
