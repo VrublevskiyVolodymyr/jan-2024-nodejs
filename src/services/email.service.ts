c7308091568c759a04f92c07cebfb490d1b7a6c2
VrublevskiyVolodymyr <v637904@gmail.com>
2024-10-07 21:18:55 +0300

    change  homework_9

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,7 +1,6 @@
-import path from "node:path";
-
 import nodemailer, { Transporter } from "nodemailer";
 import hbs from "nodemailer-express-handlebars";
+import path from "path";
 
 import { configs } from "../configs/configs";
 import { emailConstant } from "../constants/email.constant";
